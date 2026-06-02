9dbf35761a747c00508a4388d4643e87293a12aa
MRSa <mrsa@myad.jp>
2026-06-02 21:11:46 +0900

    画像ボタンを長押しで、前回撮影データを取得できるようにした。

--- app/src/main/java/jp/osdn/gokigen/aira01d/ui/component/widget/RecordImageButton.kt
+++ app/src/main/java/jp/osdn/gokigen/aira01d/ui/component/widget/RecordImageButton.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,54 +26,6 @@ import androidx.navigation.NavHostController
 import jp.osdn.gokigen.aira01d.R
 import jp.osdn.gokigen.aira01d.ui.model.CameraStatusViewModel
 import jp.osdn.gokigen.aira01d.ui.model.LiveviewViewModel
-
-/*
-@Composable
-fun RecordImageButton(navController: NavHostController, liveviewViewModel: LiveviewViewModel, viewModel: CameraStatusViewModel, modifier: Modifier = Modifier)
-{
-    val haptic = LocalHapticFeedback.current
-
-    val bitmap by liveviewViewModel.lastCapturedImage.collectAsStateWithLifecycle()
-
-    // ----- ステータスに合わせてアイコンをと色を決める
-    val imageBitmap = bitmap?.asImageBitmap()
-    val iconId = R.drawable.outline_image_24
-    val iconColor = MaterialTheme.colorScheme.primary
-
-    // ----- ボタンの表示
-    IconButton(
-        onClick = {
-            // ----- 現在未サポート
-            haptic.performHapticFeedback(HapticFeedbackType.ContextClick)
-        },
-        enabled = false,
-        modifier = modifier.size(96.dp)
-    ) {
-        if (imageBitmap != null) {
-            // --- 撮影画像を表示する ---
-            Card(
-                shape = RoundedCornerShape(16.dp),
-                elevation = CardDefaults.cardElevation(defaultElevation = 5.dp) // 影をつける
-            ) {
-                Image(
-                    bitmap = imageBitmap,
-                    contentDescription = "Captured image",
-                    modifier = Modifier.size(72.dp),
-                    contentScale = ContentScale.Crop
-                )
-            }
-        } else {
-            // --- 画像がないときは Iconを使用 ---
-            Icon(
-                painter = painterResource(iconId),
-                contentDescription = "Record image",
-                tint = iconColor,
-                modifier = Modifier.size(60.dp)
-            )
-        }
-    }
-}
-*/
 
 @Composable
 fun RecordImageButton(
