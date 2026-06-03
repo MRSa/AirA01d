f9237422c0169834573c799b0eb70d6568f465b1
MRSa <mrsa@myad.jp>
2026-06-03 22:16:31 +0900

    バージョン番号のみ更新。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "jp.osdn.gokigen.aira01d"
         minSdk = 24
         targetSdk = 37
-        versionCode = 100101
-        versionName = "1.1.1"
+        versionCode = 100500
+        versionName = "1.5.0"
     }
 
     buildTypes {
